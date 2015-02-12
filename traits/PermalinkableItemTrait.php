50d8143c250737c97946b7d7762787bfab659adc
Fredrik Wollsén <fredrik@neam.se>
2015-02-12 11:24:21 +0200

    Files push to s3 at their canonical locations + canonical file route parametrized relation

--- traits/PermalinkableItemTrait.php
+++ traits/PermalinkableItemTrait.php
@@ -2,6 +2,9 @@
 
 namespace neam\yii_permalinkable_items_core\traits;
 
+use P3Media;
+use Yii;
+
 /**
  * PermalinkableItemTrait
  */
@@ -18,17 +21,72 @@ trait PermalinkableItemTrait
         if ($config["routeClass"] == "Route") {
             return array(
                 'routes' => array(self::HAS_MANY, 'Route', array('id' => 'node_id'), 'through' => 'node'),
-                'canonicalRoute' => array(self::HAS_ONE, 'Route', array('id' => 'node_id'), 'through' => 'node', 'on' => 'canonicalRoute.canonical = 1'),
+                'canonicalRoute' => array(self::HAS_ONE, 'Route', array('id' => 'node_id'), 'through' => 'node', 'on' => 'canonical = 1'),
             );
         }
         if ($config["routeClass"] == "FileRoute") {
             return array(
                 'fileRoutes' => array(self::HAS_MANY, 'FileRoute', array('id' => 'node_id'), 'through' => 'node'),
-                'canonicalFileRoutes' => array(self::HAS_MANY, 'FileRoute', array('id' => 'node_id'), 'through' => 'node', 'on' => 'canonicalFileRoute.canonical = 1'),
+                'canonicalFileRoutes' => array(self::HAS_MANY, 'FileRoute', array('id' => 'node_id'), 'through' => 'node', 'on' => 'canonical = 1'),
                 'canonicalFileRoute' => array(self::HAS_ONE, 'FileRoute', array('id' => 'node_id'), 'through' => 'node', 'on' => 'canonicalFileRoute.canonical = 1', 'condition' => 'file_route_attribute_ref = :file_route_attribute_ref'),
             );
         }
         return array();
+    }
+
+    public function withCanonicalFileRoute($file_route_attribute_ref)
+    {
+        $this->getDbCriteria()->mergeWith(array(
+            'with' => 'canonicalFileRoute',
+            'params' => array(':file_route_attribute_ref' => $file_route_attribute_ref)
+        ));
+        return $this;
+    }
+
+    /**
+     * Pushes files to their canonical file routes
+     */
+    public function pushFilesToS3()
+    {
+
+        $results = [];
+
+        foreach ($this->canonicalFileRoutes as $canonicalFileRoute) {
+
+            /** @var P3Media $file */
+            $file = $this->{$canonicalFileRoute->file_route_attribute_ref};
+
+            /** @var \EAmazonS3ResourceManager $resourceManager */
+            $resourceManager = Yii::app()->getComponent('publicFilesResourceManager');
+            $filePathS3 = $canonicalFileRoute->route;
+            $filePath = $file->getFullPath();
+
+            $fileSavedToAmazon = null;
+            /*
+            if ($resourceManager->getIsFileExists($filePathS3)) {
+                // TODO: Ability to check hash if contents has changed
+                $fileSavedToAmazon = "(already in place at $filePathS3)";
+            } else {
+            */
+            // Always save file, since the contents may have changed
+            $fileSavedToAmazon = $resourceManager->saveFile(null, $filePathS3, ['SourceFile' => $filePath]);
+            /*
+            }
+            */
+
+            // Update P3 Media
+            $file->s3_bucket = $resourceManager->bucket;
+            $file->s3_path = $filePathS3;
+            if (!$file->save()) {
+                throw new \CException("Could not save file metadata");
+            }
+
+            $results[$canonicalFileRoute->route] = $fileSavedToAmazon;
+
+        }
+
+        return $results;
+
     }
 
 }
